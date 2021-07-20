6f516c2a7730bee15d4b5529015789c5da9c7ebd
mriise <mark.riise26@gmail.com>
2021-07-20 12:57:14 -0700

    include a task print

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use panic_halt as _;
 use rtic::app;
 
-#[app(device = stm32f2xx_hal::stm32)]
+#[app(device = stm32f2xx_hal::stm32, dispatchers = [EXTI0])]
 mod app {
     use rtt_target::{rprintln, rtt_init_print};
 
@@ -18,6 +18,14 @@ mod app {
     fn init(_: init::Context) -> (Shared, Local, init::Monotonics) {
         rtt_init_print!();
         rprintln!("init");
+
+        print::spawn().ok();
+
         (Shared {}, Local {}, init::Monotonics {})
+    }
+
+    #[task]
+    fn print(_: print::Context) {
+        rprintln!("Hello?");
     }
 }
